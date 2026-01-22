95177f64e1bfa31e4c6565ac3f9812758420f3c8
perumalponnusamy88 <perumalponnusamy@gmail.com>
2026-01-22 12:37:33 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -35,8 +35,12 @@ function rebuildProblemIndex() {
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
 
-    // Accept old FB-* IDs and new ACCOUNT_MM-YY IDs
-    if (key && (key.startsWith("FB-") || key.includes("_"))) {
+    // Accept ONLY valid problem IDs
+    // Format: ACCOUNT_MM-YY or FB-xxxxx
+    if (
+      /^FB-\d+$/.test(key) ||
+      /^[A-Z0-9]+_\d{2}-\d{2}$/.test(key)
+    ) {
       index.push(key);
     }
   }
@@ -44,3 +48,4 @@ function rebuildProblemIndex() {
   localStorage.setItem("problemIndex", JSON.stringify(index));
   return index;
 }
+
