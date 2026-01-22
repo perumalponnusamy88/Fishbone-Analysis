b93f79602557e5a3207af95f3d94dbd6b9c492c6
perumalponnusamy88 <perumalponnusamy@gmail.com>
2026-01-22 10:33:01 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -6,3 +6,14 @@ function loadFromStorage(key) {
   const raw = localStorage.getItem(key);
   return raw ? JSON.parse(raw) : null;
 }
+function getProblemIndex() {
+  return JSON.parse(localStorage.getItem("problemIndex") || "[]");
+}
+
+function addToProblemIndex(pid) {
+  const index = getProblemIndex();
+  if (!index.includes(pid)) {
+    index.push(pid);
+    localStorage.setItem("problemIndex", JSON.stringify(index));
+  }
+}
